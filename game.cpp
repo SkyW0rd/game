0fa9a4011dbe417e3c21b0b4630c634665bf0945
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-12-12 00:07:51 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -13,30 +13,31 @@ Game::Game(int x, int y, int map_w, int map_h, QString map_src):QGraphicsScene(x
     map_width = map_w;
     map_height = map_h;
     map = new GameObject**[map_height];
-
-
+    ticket = new Ticket(GameObject::None);
     for(int i = 0; i < map_height; i++)
     {
+        QVector<Ticket::ObjectType> tempVec;
+        QVector<Ticket*> tmpTicket;
         map[i] = new GameObject*[map_width];
         for(int j = 0; j < map_width; j++)
         {
             map[i][j] = nullptr;
+            tempVec.push_back(GameObject::None);
+            tmpTicket.push_back(nullptr);
         }
+        ticket->cardMap.push_back(tempVec);
+        ticket->cards.push_back(tmpTicket);
     }
-    QPixmap earthpix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\earth1.png");
-    QPixmap fieldpix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\field.png");
-    QPixmap floorpix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\floor1.png");
-    QPixmap wallpix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\wall.png");
-    QPixmap doorpix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\door.png");
-    QPixmap winplacepix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\winplace.png");
-    QPixmap inventorypix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\inventory.png");
-    QPixmap heartpix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\heart.png");
-    QPixmap facepix("C:\\Users\\Zalman N3\\Desktop\\game\\game\\res\\image\\head.png");
+    QPixmap earthpix(":/image/earth1.png");
+    QPixmap fieldpix(":/image/field.png");
+    QPixmap floorpix(":/image/floor1.png");
+    QPixmap wallpix(":/image/wall.png");
+    QPixmap doorpix(":/image/door.png");
+    QPixmap winplacepix(":/image/winplace.png");
+    QPixmap heartpix(":/image/heart.png");
+    QPixmap facepix(":/image/head.png");
     QFile mapfile(map_src);
     mapfile.open(QIODevice::ReadOnly | QIODevice::Text);
-
-    player = new Player();
-
     for(int i = 0; i < map_h; i++)
     {
         QByteArray line  = mapfile.readLine();
@@ -44,12 +45,25 @@ Game::Game(int x, int y, int map_w, int map_h, QString map_src):QGraphicsScene(x
         {
             int tmp_x = x + (j * W);
             int tmp_y = y + (i * W);
+            QVector<int> temp_x_y_Inv;
             switch(line[j])
             {
                 case '0':
                     map[i][j] = new GameObject(GameObject::Air, earthpix);
                     map[i][j]->setPos(tmp_x, tmp_y);
                     addItem(map[i][j]);
+                    if(qrand() % ((3 + 1) - 0) + 0 == 1 && ticket->tickets != 0)
+                    {
+                        ticket->tickets--;
+                        card = new Ticket(GameObject::None);
+                        card->game = this;
+                        card->setZValue(1);
+                        card->setPos(tmp_x, tmp_y);
+                        addItem(card);
+                        map[i][j] = card;
+                        ticket->cardMap[i][j] = ticket->randTicket();
+                        ticket->cards[i][j] = card;
+                    }
                     break;
                 case '1':
                     map[i][j] = new GameObject(GameObject::Wall, wallpix);
@@ -65,6 +79,18 @@ Game::Game(int x, int y, int map_w, int map_h, QString map_src):QGraphicsScene(x
                     map[i][j] = new GameObject(GameObject::Air, floorpix);
                     map[i][j]->setPos(tmp_x, tmp_y);
                     addItem(map[i][j]);
+                    if(qrand() % ((2 + 1) - 0) + 0 == 1 && ticket->tickets != 0)
+                    {
+                        ticket->tickets--;
+                        card = new Ticket(GameObject::Ticket);
+                        card->game = this;
+                        card->setZValue(1);
+                        card->setPos(tmp_x, tmp_y);
+                        addItem(card);
+                        map[i][j] = card;
+                        ticket->cardMap[i][j] = ticket->randTicket();
+                        ticket->cards[i][j] = card;
+                    }
                     break;
                 case '5':
                     map[i][j] = new GameObject(GameObject::Door, doorpix);
@@ -72,11 +98,20 @@ Game::Game(int x, int y, int map_w, int map_h, QString map_src):QGraphicsScene(x
                     addItem(map[i][j]);
                     break;
                 case '9':
-                    map[i][j] = new GameObject(GameObject::Wall, inventorypix);
-                    map[i][j]->setPos(tmp_x, tmp_y);
-                    addItem(map[i][j]);
-                    break;
 
+                    card = new Ticket(GameObject::Gun);
+                    card->game = this;
+                    card->setZValue(1);
+                    card->setPos(tmp_x, tmp_y);
+                    addItem(card);
+                    map[i][j] = card;
+                    ticket->cardMap[i][j] = GameObject::Gun;
+                    ticket->cards[i][j] = card;
+                    qDebug() << tmp_x << " " << tmp_y;
+                    temp_x_y_Inv.push_back(i);
+                    temp_x_y_Inv.push_back(j);
+                    x_y_Inv.push_back(temp_x_y_Inv);
+                    break;
                 case 'p':
                     map[i][j] = new GameObject(GameObject::Air, earthpix);
                     map[i][j]->setPos(tmp_x, tmp_y);
@@ -107,7 +142,6 @@ Game::Game(int x, int y, int map_w, int map_h, QString map_src):QGraphicsScene(x
                     map[i][j]->setPos(tmp_x, tmp_y);
                     addItem(map[i][j]);
                     break;
-
             }
             if(map[i][j])
             {
@@ -117,14 +151,160 @@ Game::Game(int x, int y, int map_w, int map_h, QString map_src):QGraphicsScene(x
         }
     }
 }
+void Game::mousePressEvent(QGraphicsSceneMouseEvent *e)
+{
+    if(stat == Game::Question)
+    {
+        if(e->scenePos().x() >= 640 && e->scenePos().x() <= 671 && e->scenePos().y() >= 224 && e->scenePos().y() <= 255)
+        {
+            int X = (static_cast<int>(e->scenePos().x()) - geo_x) / W;
+            int Y = (static_cast<int>(e->scenePos().y()) - geo_y) / W;
+            if(ticket->cardMap[Y][X] == GameObject::Gun)
+            {
+                player->addToInv(ticket->cardMap[geo_y][geo_x], Y, X, 0);
+            }
+            else if (ticket->cardMap[Y][X] != ticket->cardMap[geo_y][geo_x])
+            {
+                GameObject::ObjectType type = ticket->cardMap[geo_y][geo_x];
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[Y][X];
+                ticket->cardMap[Y][X] = type;
+                player->vecIn[0] = type;
+
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[geo_y][geo_x];
+                ticket->cards[geo_y][geo_x]->setPixmap(ticket->animCard[ticket->cardMap[geo_y][geo_x]][0]);
+
+                ticket->cardMap[Y][X] = ticket->cardMap[Y][X];
+                ticket->cards[Y][X]->setPixmap(ticket->animInv[ticket->cardMap[Y][X]][0]);
+            }
+            stat = Game::Playing;
+        }
+        if(e->scenePos().x() >= 672 && e->scenePos().x() <= 703 && e->scenePos().y() >= 224 && e->scenePos().y() <= 255)
+        {
+            int X = (static_cast<int>(e->scenePos().x()) - geo_x) / W;
+            int Y = (static_cast<int>(e->scenePos().y()) - geo_y) / W;
+            if(ticket->cardMap[Y][X] == GameObject::Gun)
+            {
+                player->addToInv(ticket->cardMap[geo_y][geo_x], Y, X, 1);
+            }
+            else if (ticket->cardMap[Y][X] != ticket->cardMap[geo_y][geo_x])
+            {
+                GameObject::ObjectType type = ticket->cardMap[geo_y][geo_x];
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[Y][X];
+                ticket->cardMap[Y][X] = type;
+                player->vecIn[1] = type;
+
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[geo_y][geo_x];
+                ticket->cards[geo_y][geo_x]->setPixmap(ticket->animCard[ticket->cardMap[geo_y][geo_x]][0]);
+
+                ticket->cardMap[Y][X] = ticket->cardMap[Y][X];
+                ticket->cards[Y][X]->setPixmap(ticket->animInv[ticket->cardMap[Y][X]][0]);
+            }
+            stat = Game::Playing;
+        }
+        else if(e->scenePos().x() >= 704 && e->scenePos().x() <= 735 && e->scenePos().y() >= 224 && e->scenePos().y() <= 255)
+        {
+            int X = (static_cast<int>(e->scenePos().x()) - geo_x) / W;
+            int Y = (static_cast<int>(e->scenePos().y()) - geo_y) / W;
+            if(ticket->cardMap[Y][X] == GameObject::Gun)
+            {
+                player->addToInv(ticket->cardMap[geo_y][geo_x], Y, X, 2);
+            }
+            else if (ticket->cardMap[Y][X] != ticket->cardMap[geo_y][geo_x])
+            {
+                GameObject::ObjectType type = ticket->cardMap[geo_y][geo_x];
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[Y][X];
+                ticket->cardMap[Y][X] = type;
+                player->vecIn[2] = type;
+
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[geo_y][geo_x];
+                ticket->cards[geo_y][geo_x]->setPixmap(ticket->animCard[ticket->cardMap[geo_y][geo_x]][0]);
+
+                ticket->cardMap[Y][X] = ticket->cardMap[Y][X];
+                ticket->cards[Y][X]->setPixmap(ticket->animInv[ticket->cardMap[Y][X]][0]);
+            }
+            stat = Game::Playing;
+        }
+        else if(e->scenePos().x() >= 736 && e->scenePos().x() <= 767 && e->scenePos().y() >= 224 && e->scenePos().y() <= 255)
+        {
+            int X = (static_cast<int>(e->scenePos().x()) - geo_x) / W;
+            int Y = (static_cast<int>(e->scenePos().y()) - geo_y) / W;
+            if(ticket->cardMap[Y][X] == GameObject::Gun)
+            {
+                player->addToInv(ticket->cardMap[geo_y][geo_x], Y, X, 3);
+            }
+            else if (ticket->cardMap[Y][X] != ticket->cardMap[geo_y][geo_x])
+            {
+                GameObject::ObjectType type = ticket->cardMap[geo_y][geo_x];
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[Y][X];
+                ticket->cardMap[Y][X] = type;
+                player->vecIn[3] = type;
+
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[geo_y][geo_x];
+                ticket->cards[geo_y][geo_x]->setPixmap(ticket->animCard[ticket->cardMap[geo_y][geo_x]][0]);
+
+                ticket->cardMap[Y][X] = ticket->cardMap[Y][X];
+                ticket->cards[Y][X]->setPixmap(ticket->animInv[ticket->cardMap[Y][X]][0]);
+            }
+            stat = Game::Playing;
+        }
+        else if(e->scenePos().x() >= 768 && e->scenePos().x() <= 799 && e->scenePos().y() >= 224 && e->scenePos().y() <= 255)
+        {
+            int X = (static_cast<int>(e->scenePos().x()) - geo_x) / W;
+            int Y = (static_cast<int>(e->scenePos().y()) - geo_y) / W;
+            if(ticket->cardMap[Y][X] == GameObject::Gun)
+            {
+                player->addToInv(ticket->cardMap[geo_y][geo_x], Y, X, 4);
+            }
+            else if (ticket->cardMap[Y][X] != ticket->cardMap[geo_y][geo_x])
+            {
+                GameObject::ObjectType type = ticket->cardMap[geo_y][geo_x];
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[Y][X];
+                ticket->cardMap[Y][X] = type;
+                player->vecIn[4] = type;
+
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[geo_y][geo_x];
+                ticket->cards[geo_y][geo_x]->setPixmap(ticket->animCard[ticket->cardMap[geo_y][geo_x]][0]);
+
+                ticket->cardMap[Y][X] = ticket->cardMap[Y][X];
+                ticket->cards[Y][X]->setPixmap(ticket->animInv[ticket->cardMap[Y][X]][0]);
+            }
+            stat = Game::Playing;
+        }
+        else if(e->scenePos().x() >= 800 && e->scenePos().x() <= 831 && e->scenePos().y() >= 224 && e->scenePos().y() <= 255)
+        {
+            int X = (static_cast<int>(e->scenePos().x()) - geo_x) / W;
+            int Y = (static_cast<int>(e->scenePos().y()) - geo_y) / W;
+            if(ticket->cardMap[Y][X] == GameObject::Gun)
+            {
+                player->addToInv(ticket->cardMap[geo_y][geo_x], Y, X, 5);
+            }
+            else if (ticket->cardMap[Y][X] != ticket->cardMap[geo_y][geo_x])
+            {
+                GameObject::ObjectType type = ticket->cardMap[geo_y][geo_x];
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[Y][X];
+                ticket->cardMap[Y][X] = type;
+                player->vecIn[5] = type;
+
+                ticket->cardMap[geo_y][geo_x] = ticket->cardMap[geo_y][geo_x];
+                ticket->cards[geo_y][geo_x]->setPixmap(ticket->animCard[ticket->cardMap[geo_y][geo_x]][0]);
+
+                ticket->cardMap[Y][X] = ticket->cardMap[Y][X];
+                ticket->cards[Y][X]->setPixmap(ticket->animInv[ticket->cardMap[Y][X]][0]);
+            }
+            stat = Game::Playing;
+        }
+    }
+}
 void Game::start()
 {
 
 }
+
 void Game::stop()
 {
 
 }
+
 int Game::giveAction()
 {
     return(qrand()%((4 + 1) - 1) + 1);
